c1dcc1eaba14d6f9f6f4c237a240b9c15cb1bb19
yankogp <146089512+yankogp@users.noreply.github.com>
2024-02-14 15:43:07 +0200

    2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>First Commit...!!!</span>
+          <span style={{ color: 'red' }}>Second Commit...!!!</span>
         </div>
       </header>
     </div>
